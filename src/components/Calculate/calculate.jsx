2cd3a27643d6e07ec5a02cdb99fb73bb90588516
Ishika Mishra <ishiishikamishra07@gmail.com>
2024-03-22 21:19:42 +0530

    changes in calculate component

--- src/components/Calculate/calculate.jsx
+++ src/components/Calculate/calculate.jsx
@@ -52,6 +52,7 @@ export default function Calculate() {
                 break;
             case '=':
                 setDisplayValue(previousNumber)
+                break;
             default:
                 setDisplayValue("Unknown error");
                 break;
@@ -134,10 +135,10 @@ export default function Calculate() {
             </div>
             <div className="footer">
                 <div className="int">
-                    <a href="https://github.com/saurav-tiwari03" target="_blank"><i class="fa-brands fa-github">Github</i></a>
+                    <a href="https://github.com/saurav-tiwari03" target="_blank" without rel="noreferrer"><i class="fa-brands fa-github">Github</i></a>
                 </div>
                 <div className="int">
-                    <a href="https://my-portfolio-03.netlify.com" target="_blank"><i class="fa-solid fa-user">Porfile</i></a>
+                    <a href="https://my-portfolio-03.netlify.com" target="_blank" without rel="noreferrer"><i class="fa-solid fa-user">Porfile</i></a>
                 </div>
             </div>
             </div>
